a1d08d55213f850eb88be9207fadb653b02ca344
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-07-10 15:09:30 +0530

    cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req,res,next) => {
 app.use('/api',router)
 
 // cors
-//https://kaleidoscopic-jalebi-2cc6e1.netlify.app/
+//https://kaleidoscopic-jalebi-2cc6e1.netlify.app
 app.use(cors({
     origin:'https://kaleidoscopic-jalebi-2cc6e1.netlify.app',
     credentials:true,
