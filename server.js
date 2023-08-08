66326262023ec7906ec9a2dcd29ff36d6f024ed2
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-08 13:00:52 +0530

    api

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
-const express  = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
-const bodyParser = require('body-parser');
-const db = require('./db')
-const PORT = process.env.PORT || 5000
-const router = require('./routers/Index')
+const bodyParser = require("body-parser");
+const db = require("./db");
+const PORT = process.env.PORT || 5000;
+const router = require("./routers/Index");
 
 // db connection
-db.connect()
+db.connect();
 
 // middleware
-app.use(bodyParser.json({limit:"50mb"}))
-app.use(bodyParser.urlencoded({ extended:true,limit:"50mb"}))
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use(express.json())
+app.use(express.json());
 
 // headers
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    next()
-})
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 // api
-app.use('/api',router)
+app.use("/api", router);
 
 // cors
-app.use(cors())
-
+app.use(cors(
+  {
+     origin: "https://kaleidoscopic-jalebi-2cc6e1.netlify.app",
+     redentials: true,
+   }
+  )
+);
+// {
+//     origin:"https://kaleidoscopic-jalebi-2cc6e1.netlify.app",
+//     redentials:true
+// }
 // listning
-app.listen(PORT , ()=>{
-    console.log("Server lisning on port 5000")
-})
+app.listen(PORT, () => {
+  console.log("Server lisning on port 5000");
+});
