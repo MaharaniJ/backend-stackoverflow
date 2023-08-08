eaa83a61f473a9c64238e76cd9bb9d83557afb69
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-08 15:12:28 +0530

    prettier

--- Models/Question.js
+++ Models/Question.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
-    title: String,
-    body: String,
-    tags : [],
-    createdAt: {
-        type:Date,
-        default : Date.now(),
-    },
-    user:Object,
-    views: {type :Number, default :0},
-    answers: {type :Number, default :0},
-})
+  title: String,
+  body: String,
+  tags: [],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: Object,
+  views: { type: Number, default: 0 },
+  answers: { type: Number, default: 0 },
+});
 
-module.exports=mongoose.model("Question",questionSchema);
+module.exports = mongoose.model("Question", questionSchema);
--- routers/Index.js
+++ routers/Index.js
@@ -1,15 +1,13 @@
-const express=require('express')
-const router=express.Router()
-const questionRouter = require('./Question')
-const answerRouter = require('./Answer')
+const express = require("express");
+const router = express.Router();
+const questionRouter = require("./Question");
+const answerRouter = require("./Answer");
 
-
-router.get('/',(req, res) =>{
-    res.send("Welcome to stack clone")
+router.get("/", (req, res) => {
+  res.send("Welcome to stack clone");
 });
 
-router.use('/question',questionRouter);
-router.use('/answer',answerRouter);
-
+router.use("/question", questionRouter);
+router.use("/answer", answerRouter);
 
 module.exports = router;
